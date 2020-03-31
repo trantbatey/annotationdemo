3aa56e5c4d0e26d782a27519fc7174b3bfa8c68a
trantbatey <trantbatey@gmail.com>
2020-03-31 00:11:40 -0500

    Added the files.

---
+++ src/main/java/com/codeup/annotationdemo/AnnotationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.annotationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationdemoApplication.class, args);
+    }
+
+}
